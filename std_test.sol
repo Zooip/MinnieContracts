37ffb715428f3b512f89086561964aaff602f3bd
Alexandre Hajjar <alexandre.hajjar@gmail.com>
2017-06-03 22:33:16 +0200

    Dappfile & tests for std.sol

---
+++ std_test.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.9;
+
+import 'dapple/test.sol';
+
+import 'std.sol';
+
+contract ownedTest is Test {
+  Tester proxy_tester;
+
+  owned owned_contract;
+  event OwnerChanged(
+    address newOwner,
+    address previousOwner
+  );
+
+  function setUp() {
+    owned_contract = new owned();
+    proxy_tester = new Tester();
+    proxy_tester._target(owned_contract);
+  }
+
+  function testOwnerIsSetFromSender() {
+    assertEq(address(this), owned_contract.owner());
+  }
+
+  function testOwnerIsSetFromChange() {
+    owned_contract.changeOwner(address(1));
+    assertEq(address(1), owned_contract.owner());
+  }
+
+
+  function testEventIsSentAtChange() {
+    expectEventsExact(owned_contract);
+    OwnerChanged(address(1), address(this));
+    owned_contract.changeOwner(address(1));
+  }
+}
